d861bc13037088501828bf3de32c161a31082525
Yanislav Igonin <yanislavdabest@gmail.com>
2020-12-02 02:02:15 +0300

    feat: add metrics path config

--- src/common/config/app.config.ts
+++ src/common/config/app.config.ts
@@ -1,6 +1,9 @@
 const AppConfig = {
   env: process.env.NODE_ENV || 'development',
   release: process.env.CI_COMMIT_TAG || 'development',
+  metrics: {
+    path: process.env.METRICS_PATH || '/metrics',
+  },
 };
 
 export { AppConfig };
