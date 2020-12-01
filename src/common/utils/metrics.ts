7b9cf84eee04457994c41adae0aeeec5b64a52f0
Yanislav Igonin <yanislavdabest@gmail.com>
2020-12-02 02:24:06 +0300

    feat: metrics wip

---
+++ src/common/utils/metrics.ts
@@ -0,0 +1,52 @@
+import { Middleware } from 'koa';
+import * as client from 'prom-client';
+import { AppConfig } from '../config';
+
+interface MetricsOptions {
+  appName: string;
+  path: string;
+  totalRequestsCounterName: string;
+}
+
+const defaultOptions: Omit<MetricsOptions, 'appName' | 'path'> = {
+  totalRequestsCounterName: 'bot_total_requests',
+};
+
+class Metrics {
+  private options: MetricsOptions;
+  private registry: client.Registry;
+  private koaMiddleware: Middleware;
+  private totalRequestsCounter: client.Counter<'botName'>;
+
+  constructor(options: typeof AppConfig.metrics) {
+    this.options = {
+      ...defaultOptions,
+      path: options.path,
+      appName: options.appName,
+    };
+    this.registry = new client.Registry();
+    this.koaMiddleware = async (ctx, next) => {
+      if (ctx.req.method === 'GET' && ctx.req.url === this.options.path) {
+        ctx.body = this.registry.metrics();
+      } else {
+        await next();
+      }
+    };
+
+    this.totalRequestsCounter = new client.Counter({
+      name: this.options.totalRequestsCounterName,
+      help: 'Total bot requests',
+      labelNames: ['botName'],
+    });
+
+    this.registry.registerMetric(this.totalRequestsCounter);
+  }
+
+  get middleware() {
+    return this.koaMiddleware;
+  }
+}
+
+const metrics = new Metrics(AppConfig.metrics);
+
+export { metrics };
--- src/modules/api/middlewares/index.ts
+++ src/modules/api/middlewares/index.ts
@@ -1,7 +1,10 @@
 import * as koaBody from 'koa-body';
 import * as helmet from 'koa-helmet';
 
+import { metrics } from '../../../common/utils';
+
 export const middlewares = [
   koaBody(),
   helmet(),
+  metrics.middleware,
 ];
