54bdadfac2b34e27f8a0dd404dbdc12493239aa8
Yanislav Igonin <yanislavdabest@gmail.com>
2020-12-02 02:05:57 +0300

    feat: add metrics app name config

--- src/common/config/app.config.ts
+++ src/common/config/app.config.ts
@@ -2,6 +2,7 @@ const AppConfig = {
   env: process.env.NODE_ENV || 'development',
   release: process.env.CI_COMMIT_TAG || 'development',
   metrics: {
+    appName: process.env.APP_NAME || 'typescript-telegram-bot-template',
     path: process.env.METRICS_PATH || '/metrics',
   },
 };
