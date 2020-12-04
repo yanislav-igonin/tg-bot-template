0227d89ada91e46c4dd7f627fd7eb59a6fe4a092
Yanislav Igonin <yanislavdabest@gmail.com>
2020-12-05 02:32:27 +0300

    fix: bot requests without auth

--- src/modules/api/middlewares/auth.ts
+++ src/modules/api/middlewares/auth.ts
@@ -1,12 +1,15 @@
 import { Context, Next } from 'koa';
-import { AppConfig } from '../../../common/config';
+import * as config from '../../../common/config';
 
 export const auth = async (ctx: Context, next: Next) => {
-  const { authorization }: { authorization: string } = ctx.request.header;
-  // TODO: add http error
-  if (authorization === undefined) throw new Error('Auth token required.');
-  // TODO: add http error
-  const token = authorization.split(' ')[1];
-  if (token !== AppConfig.apiToken) throw new Error('Wrong auth token.');
+  if (ctx.url !== config.TelegramConfig.webhook.path) {
+    const { authorization }: { authorization: string } = ctx.request.header;
+    // TODO: add http error
+    if (authorization === undefined) throw new Error('Auth token required.');
+    // TODO: add http error
+    const token = authorization.split(' ')[1];
+    if (token !== config.AppConfig.apiToken) throw new Error('Wrong auth token.');
+  }
+
   await next();
 };
