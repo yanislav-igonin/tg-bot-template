26e8bd909484fe3568ce8fb815e551c60200ed5d
Yanislav Igonin <yanislavdabest@gmail.com>
2020-12-05 03:23:52 +0300

    fix: naming

--- src/modules/api/middlewares/auth.ts
+++ src/modules/api/middlewares/auth.ts
@@ -1,13 +1,13 @@
 import { Middleware } from 'koa';
-import * as config from '../../../common/config';
+import * as Config from '../../../common/config';
 import { UnauthorizedError, ForbiddenError } from '../errors';
 
 export const auth: Middleware = async (ctx, next) => {
-  if (ctx.url !== config.TelegramConfig.webhook.path) {
+  if (ctx.url !== Config.TelegramConfig.webhook.path) {
     const { authorization }: { authorization: string } = ctx.request.header;
     if (authorization === undefined) throw new UnauthorizedError();
     const token = authorization.split(' ')[1];
-    if (token !== config.AppConfig.apiToken) throw new ForbiddenError();
+    if (token !== Config.AppConfig.apiToken) throw new ForbiddenError();
   }
 
   await next();
