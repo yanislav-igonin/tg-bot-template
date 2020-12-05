34908f724dbd76d15bc3c0307bc11547b4ee8242
Yanislav Igonin <yanislavdabest@gmail.com>
2020-12-06 02:05:21 +0300

    lint

--- src/modules/api/middlewares/botHandler.ts
+++ src/modules/api/middlewares/botHandler.ts
@@ -8,10 +8,10 @@ export const botHandler = (
 ): Middleware => (ctx, next) => {
   const { method, url } = ctx;
   const { path } = config.telegramConfig.webhook;
-
   if (method !== 'POST' && url !== path) {
     return next();
   }
+
   ctx.status = 200;
   // @ts-ignore
   return bot.telegraf.handleUpdate(ctx.request.body, ctx.response);
