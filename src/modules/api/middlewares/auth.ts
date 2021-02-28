86c6ea15017e80f95e46d7ca96c673d61c53e347
Yanislav Igonin <yanislavdabest@gmail.com>
2021-03-01 02:07:45 +0300

    feat: update packages

--- src/modules/api/middlewares/auth.ts
+++ src/modules/api/middlewares/auth.ts
@@ -5,7 +5,7 @@ import { UnauthorizedError, ForbiddenError } from '../errors';
 export const auth: Middleware = async (ctx, next) => {
   if (ctx.url !== config.telegramConfig.webhook.path) {
     // Authorization: Bearer {secret}
-    const { authorization }: { authorization: string } = ctx.request.header;
+    const { authorization } = ctx.request.header;
     if (authorization === undefined) throw new UnauthorizedError();
     const token = authorization.split(' ')[1];
     if (token !== config.appConfig.apiToken) throw new ForbiddenError();
