b383ae60a45fa2e4aa74464b8a56588aa915cdf0
Yanislav Igonin <yanislavdabest@gmail.com>
2023-02-27 22:35:12 +0200

    change imports

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,6 @@
-import { config } from '../config';
-import { type User } from '../database';
-import { database } from '../database';
+import { config } from '@/config';
+import { type User } from '@/database';
+import { database } from '@/database';
 
 export const get = async (id: number) =>
   await database.user.findUnique({ where: { id } });
