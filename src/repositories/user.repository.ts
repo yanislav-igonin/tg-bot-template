ea1e2812382af6bb660883fb5d527fdb7ff24966
Yanislav Igonin <yanislavdabest@gmail.com>
2023-05-10 10:44:07 +0300

    update create user

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -5,7 +5,7 @@ import { database } from '@/database';
 export const get = async (id: string) =>
   await database.user.findUnique({ where: { id } });
 
-export const create = async (data: Omit<User, 'createdAt'>) =>
+export const create = async (data: Omit<User, 'createdAt' | 'isAllowed'>) =>
   await database.user.create({ data });
 
 export const hasAccess = (username: string) =>
